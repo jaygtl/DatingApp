d02e93ed06ddae68cec0f8487cf4734821e06827
Jay Rao <jay.rao@gatewaytechnolabs.com>
2019-03-07 22:29:42 +0100

    added jwthelper and alertify

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators'
+import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 baseUrl = 'http://localhost:7000/api/auth/';
+jwtHelper = new JwtHelperService();
+decodedToken :any;
 constructor(private http: HttpClient) { }
 
 login(model: any){
@@ -16,12 +19,19 @@ login(model: any){
       if(user)
       {
         localStorage.setItem('token', user.token);
+        this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        
       }
     })
   )
 }
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model)
+}
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !this.jwtHelper.isTokenExpired(token);
 }
 
 }
