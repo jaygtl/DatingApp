b953139da39073fbae5111a7a8933a9157f2fa48
Jay Rao <jay.rao@gatewaytechnolabs.com>
2019-07-02 22:37:19 +0200

    messagecontroller issue fixed, message inbox outbox

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using AutoMapper;
+using DatingApp.API.Controllers;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 
@@ -30,6 +31,13 @@ namespace DatingApp.API.Helpers
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();  
             CreateMap<UserForRegisterDto, User>();
+            CreateMap<MessageForCreationDto, Message>();
+            CreateMap<Message,MessageToReturnDto>()
+            .ForMember(m => m.SenderPhotoUrl, opt => opt
+                .MapFrom(u =>u.Sender.Photos.FirstOrDefault(p =>p.IsMain).Url))
+            .ForMember(m => m.RecipientPhotoUrl, opt => opt
+                .MapFrom(u =>u.Recipient.Photos.FirstOrDefault(p =>p.IsMain).Url));
+
         }
     }
 }
